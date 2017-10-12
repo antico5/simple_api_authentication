4d6dbe751541ba41bb9d3284414458b788b7076d
Armando Andini <armando.andini@hotmail.com>
2017-10-12 14:48:29 -0300

    Simple rails API with authentication.

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+#config/routes.rb
+Rails.application.routes.draw do
+  #add our register route
+  post 'auth/register', to: 'users#register'
+  post 'auth/login', to: 'users#login'
+  get 'test', to: 'users#test'
+end
